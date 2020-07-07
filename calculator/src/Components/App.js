7133af94ee93ce776d996d11edaa24fe6bb86ea3
devashrlucas <devashrlucas@gmail.com>
2020-07-07 15:29:23 -0400

    Working on functions for calculations.

--- calculator/src/Components/App.js
+++ calculator/src/Components/App.js
@@ -3,47 +3,63 @@ import InputForm from './InputForm'
 
 
 function App() {
-   const [userInput, setUserInput] = useState({
+  const [userInput, setUserInput] = useState({
       start: 0,
       end: 0,
     });
+  //const [warmupWeights, setWarmupWeights] = useState({})
   const [roundedWeights, setRoundedWeights] = useState([]);
-      const calculateWeights = () => {
-        const weights = [45];
-        const weightIncrease = Number(userInput.end - userInput.start) / 4;
-        let temporaryIncrease = 0;
-        let i;
 
-        for (i = 0; i < 4; i += 1) {
-          temporaryIncrease += weightIncrease;
-          weights.push(temporaryIncrease);
-        }
-        return weights;
-      };
-    const weights = calculateWeights();
-    
-    const calculateRoundedWeights = () => {
-      const rounded = [];
-      let temporaryRounded ;
-      let i;
-      
-      for (i = 0; i < weights.length; i+= 1) {
-        temporaryRounded = Math.ceil((weights[i] / 5)) * 5;
-        rounded.push(temporaryRounded);
-      }
-      return rounded
+  const calculateWeights = () => {
+    const weights = [45];
+    const weightIncrease = Number(userInput.end - userInput.start) / 4;
+    let temporaryIncrease = 0;
+    let i;
+
+    for (i = 0; i < 4; i += 1) {
+      temporaryIncrease += weightIncrease;
+      weights.push(temporaryIncrease);
     }
-    const rounded = calculateRoundedWeights(); 
-    console.log(rounded)
+    return weights;
+  };
     
-    //setRoundedWeights(([...roundedWeights, rounded]: roundedWeights));
-  
-  
+  const weights = calculateWeights();
 
+  const calculateRoundedWeights = () => {
+    const rounded = [];
+    let temporaryRounded;
+    let i;
+    
+    for (i = 0; i < weights.length; i+= 1) {
+      temporaryRounded = Math.ceil((weights[i] / 5)) * 5;
+      rounded.push(temporaryRounded);
+    }
+    return rounded
+  }
+  const rounded = calculateRoundedWeights();  
 
+  const calculateOneSide = () => {
+    const oneSide = [];
+    let temporaryOneSide;
+    let i;
+
+    for (i = 0; i < rounded.length; i += 1) {
+      if (rounded[i] === 45) {
+        temporaryOneSide = 0;
+        oneSide.push(temporaryOneSide);
+      } else {
+        temporaryOneSide = (rounded[i] - 45) / 2;
+        oneSide.push(temporaryOneSide);
+      }
+    }
+    return oneSide;
+  }
+
+  const oneSide = calculateOneSide();
+  console.log(oneSide);
   return (
     <div>
-      <InputForm setUserInput={setUserInput} userInput={userInput} />
+      <InputForm setUserInput={setUserInput} userInput={userInput}/>
     </div>
   );
   
